97c5f7defcaf1cb51140e7c9e74e582fd60eac8f
Ubuntu <ubuntu@ip-172-31-31-224.ap-southeast-1.compute.internal>
2016-03-25 13:12:32 +0000

    修改数据库连接

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii',
     'username' => 'root',
-    'password' => 'z19890601',
+    'password' => '123456',
     'charset' => 'utf8',
 ];
