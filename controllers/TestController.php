a569a91358e1d339f0679b7e9644d4868982d391
kevin <zjynhh168@163.com>
2016-03-26 00:05:24 +0800

    添加登陆权限验证

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -8,6 +8,7 @@ use app\models\OrderSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * TestController implements the CRUD actions for Order model.
@@ -17,6 +18,17 @@ class TestController extends Controller
     public function behaviors()
     {
         return [
+        		'access' => [
+        				'class' => AccessControl::className(),//授权过滤 仅允许登陆的用户执行操作  
+        				//'only' => ['login', 'logout', 'signup'],//表示需要执行过滤的操作 为空表示全部过滤
+        				'rules' => [
+        						[
+        								'allow' => true,//表示允许执行
+        								//'actions' => ['login', 'signup'],//允许执行的方法 为空表示全部
+        								'roles' => ['@'],// 对角色授权 @表示登陆用户 ?表示游客
+        						],
+        				],
+        		],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
