023275722d8d13074325f2ce2e9c0a8e78add019
kevin <zjynhh168@163.com>
2016-03-26 12:17:35 +0800

    添加微信接入

--- modules/Weixin/controllers/DefaultController.php
+++ modules/Weixin/controllers/DefaultController.php
@@ -4,11 +4,14 @@ namespace app\modules\Weixin\controllers;
 
 use yii\web\Controller;
 
-define ( "TOKEN", "weixin" );
 class DefaultController extends Controller {
+	public $token = 'weixin';
 	public function actionIndex() {
-		$echoStr = $_GET ["echostr"];
-		$this->valid ( $echoStr );
+		$echoStr = \Yii::$app->request->get ( 'echostr' );
+		if ($echoStr) {
+			$this->valid ( $echoStr );
+		}
+		$this->responseMsg();
 		// return $this->render ( 'index' );
 	}
 	public function valid($echoStr) {
@@ -20,7 +23,7 @@ class DefaultController extends Controller {
 	}
 	private function checkSignature() {
 		// you must define TOKEN by yourself
-		if (! defined ( "TOKEN" )) {
+		if (! $this->token) {
 			throw new Exception ( 'TOKEN is not defined!' );
 		}
 		
@@ -28,7 +31,7 @@ class DefaultController extends Controller {
 		$timestamp = $_GET ["timestamp"];
 		$nonce = $_GET ["nonce"];
 		
-		$token = TOKEN;
+		$token = $this->token;
 		$tmpArr = array (
 				$token,
 				$timestamp,
@@ -43,6 +46,43 @@ class DefaultController extends Controller {
 			return true;
 		} else {
 			return false;
+		}
+	}
+	public function responseMsg() {
+		// get post data, May be due to the different environments
+		$postStr = $GLOBALS ["HTTP_RAW_POST_DATA"];
+		
+		// extract post data
+		if (! empty ( $postStr )) {
+			/*
+			 * libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+			 * the best way is to check the validity of xml by yourself
+			 */
+			libxml_disable_entity_loader ( true );
+			$postObj = simplexml_load_string ( $postStr, 'SimpleXMLElement', LIBXML_NOCDATA );
+			$fromUsername = $postObj->FromUserName;
+			$toUsername = $postObj->ToUserName;
+			$keyword = trim ( $postObj->Content );
+			$time = time ();
+			$textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";
+			if (! empty ( $keyword )) {
+				$msgType = "text";
+				$contentStr = "Welcome to wechat world!";
+				$resultStr = sprintf ( $textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr );
+				echo $resultStr;
+			} else {
+				echo "Input something...";
+			}
+		} else {
+			echo "";
+			exit ();
 		}
 	}
 }
