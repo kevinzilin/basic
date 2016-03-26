137a58139c27fd0b01e23594fc75e37e2e822396
kevin <zjynhh168@163.com>
2016-03-26 17:23:23 +0800

    修改数据库

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii',
+    'dsn' => 'mysql:host=kevin.czq5wti59vdn.ap-southeast-1.rds.amazonaws.com:3306;dbname=yii',
     'username' => 'root',
-    'password' => '123456',
+    'password' => 'abc123456',
     'charset' => 'utf8',
 ];
